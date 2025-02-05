95a95c4c7c5921edc7345102dca72402a267bb33
jitender singh <jitendersinghjatt09@gmail.com>
2025-02-05 21:52:32 +0530

    initial setup

--- IMPCOMMANDS.py
+++ IMPCOMMANDS.py
@@ -2,5 +2,11 @@
 
 -> Activate the Virtual Environment: : - source env/bin/activate
 
+-> install required package to connect with database : -pip install sqlalchemy psycopg2-binary asyncpg alembic
+  note: - sqlalchemy: ORM for handling database operations.
+        psycopg2-binary: PostgreSQL adapter for synchronous connections.
+        asyncpg: High-performance PostgreSQL driver for async operations.
+        alembic: Database migrations.
+
 
 """
--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
 # Entry point of application
+
+from fastapi import FastAPI
+
+app = FastAPI()
+@app.get("/")
+def read_root():
+    return {"message": "Hello, FastAPI!"}
