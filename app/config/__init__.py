f0016a44f8ae9bd6308b0965a6ae5ea87e5cb9ab
jitender singh <jitendersinghjatt09@gmail.com>
2025-02-16 17:28:38 +0530

    revert code

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -1,2 +1,1 @@
 from config.setting import settings as envVariables
-from config.dependencies import Dependencies
--- app/main.py
+++ app/main.py
@@ -1,11 +1,5 @@
 from fastapi import FastAPI
 from routes import mainRouter
-from config import Dependencies
-from sqlalchemy.ext.asyncio import AsyncSession
 
 app = FastAPI()
 app.include_router(mainRouter)
-@app.get("/items/")
-async def get_items(db: AsyncSession = Dependencies.db):
-    # Your logic here
-    return {"message": "Success"}
