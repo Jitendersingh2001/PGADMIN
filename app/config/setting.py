7d19a960af7cc30bc25f78dde5ce28a0ffeac25e
jitender singh <jitendersinghjatt09@gmail.com>
2025-02-16 15:24:05 +0530

    set up setting file

--- app/config/setting.py
+++ app/config/setting.py
@@ -1,26 +1,33 @@
-import os
-from dotenv import load_dotenv
+from functools import lru_cache
+from pydantic import BaseSettings, Field, SecretStr
 
-# Load environment variables from .env file
-load_dotenv()
-
-class Settings:
-    # Store DB credentials in a dictionary
-    DB: dict = {
-        "DB_HOST": os.getenv("DB_HOST", "localhost"),
-        "DB_PORT": os.getenv("DB_PORT", "5432"),
-        "DB_USER": os.getenv("DB_USER", "admin"),
-        "DB_PASS": os.getenv("DB_PASS", "admin123"),
-        "DATABASE_NAME": os.getenv("DATABASE_NAME", "pgrooms"),
-    }
-
-    # Construct DATABASE_URL for SQLAlchemy
-    DATABASE_URL: str = (
-        f"postgresql+asyncpg://{DB['DB_USER']}:{DB['DB_PASS']}@{DB['DB_HOST']}:{DB['DB_PORT']}/{DB['DATABASE_NAME']}"
-    )
+class Settings(BaseSettings):
+    # Database configuration
+    DB_HOST: str = Field(..., env="DB_HOST")
+    DB_PORT: str = Field(..., env="DB_PORT")
+    DB_USER: str = Field(..., env="DB_USER")
+    DB_PASS: SecretStr = Field(..., env="DB_PASS")
+    DATABASE_NAME: str = Field(..., env="DATABASE_NAME")
 
     # JWT Secret Key
-    JWT_SECRET_KEY: str = os.getenv("JWT_SECRET_KEY", "default_secret")
+    JWT_SECRET_KEY: SecretStr = Field(..., env="JWT_SECRET_KEY")
 
-# Create an instance of Settings
-settings = Settings()
+    # Construct DATABASE_URL for SQLAlchemy
+    @property
+    def DATABASE_URL(self) -> str:
+        return (
+            f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS.get_secret_value()}@"
+            f"{self.DB_HOST}:{self.DB_PORT}/{self.DATABASE_NAME}"
+        )
+    class Config:
+        env_file = ".env"
+
+# Create a singleton instance of Settings
+"""
+LRU  It stands for Least Recently Used (LRU) Cache
+"""
+@lru_cache()
+def get_settings() -> Settings:
+    return Settings()
+
+settings = get_settings() 
